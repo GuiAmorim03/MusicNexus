fd9f86bbecd3dbe0f84b5cf05823880a414da9d1
GuiAmorim03 <guilhermeamorim@ua.pt>
2024-05-23 19:27:15 +0100

    create a navbar component to be reused

---
+++ js/navbar.js
@@ -0,0 +1,25 @@
+document.getElementById('navbar').innerHTML= `
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: rgba(0, 0, 0, .25)">
+<a class="navbar-brand" href="/">
+    <img src="/images/utils/logo_white.png" height="40">
+</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Music</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Events</a>
+        </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+        <div class="text-light">Welcome, someone</div>
+    </form>
+</div>
+</nav>
+`
