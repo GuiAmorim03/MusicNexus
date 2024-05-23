39e8603ee3c74bfad4b871629971b85f7eff9b9b
GuiAmorim03 <guilhermeamorim@ua.pt>
2024-05-23 18:23:10 +0100

    music page

--- js/data.js
+++ js/data.js
@@ -25,6 +25,42 @@ const user_types = [
     },
 ]
 
+const videos = [
+    {
+        id: 1,
+        title: "Good Boy",
+        author: "Four & Half ft. Big Charles",
+        instrument: "Mix",
+        stars: 1427,
+        type: 'original',
+    },
+    {
+        id: 2,
+        title: "A Vida é Boa",
+        author: "Jogo Frio",
+        instrument: "Guitar",
+        stars: 5429,
+        type: 'cover',
+    },
+    {
+        id: 3,
+        title: "Canção da Preguiça",
+        author: "Bruno Marciano",
+        instrument: "Voice", 
+        stars: 342,
+        type: 'original',
+    },
+    {
+        id: 4,
+        title: "Muitas Estrelas",
+        author: "António Simões",
+        instrument: "Piano",
+        stars: 121,
+        type: 'original',
+    },
+]
+
 data = {}
 data.user_types = user_types;
+data.videos = videos;
 localStorage.setItem('musicNexus', JSON.stringify(data))
---
+++ js/pages/music.js
@@ -0,0 +1,25 @@
+data = JSON.parse(localStorage.getItem('musicNexus'))
+
+const videosRow = document.getElementById('videos')
+data.videos.forEach(video => {
+    const videoDiv = document.createElement('div')
+    videoDiv.classList.add('col-lg-4', 'col-md-6', 'col-sm-12', 'mb-3', 'mt-2');
+    videoDiv.innerHTML = `
+    <div class="card" style="border: 0;">
+        <video src="videos/video_${video.id}.mp4" class="card-img-top" alt="video: ${video.title}"></video>
+            <div class="card-body">
+                <div class="d-flex justify-content-between align-items-center">
+                    <h5 class="card-title">${video.title}</h5>
+                    <img src="./icons/${video.instrument.toLowerCase()}.svg" height="25">
+                </div>
+                <div class="text-muted d-flex justify-content-between align-items-center">
+                    <div>    
+                        <i class="fas fa-user"></i> ${video.author}
+                    </div>
+                    ${video.type}
+                </div>
+            </div>
+        </div>
+    `;
+    videosRow.appendChild(videoDiv);
+})
