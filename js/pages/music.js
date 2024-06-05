d2b5ee1daa01af27e070cc4481b2a997c7313888
GuiAmorim03 <guilhermeamorim@ua.pt>
2024-06-05 18:53:09 +0100

    video functionalities working

--- js/pages/music.js
+++ js/pages/music.js
@@ -10,15 +10,12 @@ function createVideos(instruments, type) {
     const videosRow = document.getElementById('videos')
     videosRow.innerHTML = "";
     data.videos.forEach(video => {
-        console.log(video.instrument)
-        console.log(instruments)
-        console.log(instruments.includes(video.instrument))
         if (instruments.length==0 || instruments.includes(video.instrument)) {
             if (type == "all" || video.type == type) {
                 const videoDiv = document.createElement('div')
                 videoDiv.classList.add('col-lg-4', 'col-md-12', 'col-sm-12', 'mb-3', 'mt-2');
                 videoDiv.innerHTML = `
-                    <div class="card" style="border: 0; cursor:pointer" onclick='openModal("videos/video_${video.id}.mp4", "${video.title}", "${video.author}")'>
+                    <div class="card" style="border: 0; cursor:pointer" onclick='openModal("videos/video_${video.id}.mp4", "${video.title}", "${video.author}", "${video.stars}")'>
                         <video src="videos/video_${video.id}.mp4" class="card-img-top" alt="video: ${video.title}"></video>
                             <div class="card-body">
                                 <div class="d-flex justify-content-between align-items-top">
@@ -78,11 +75,23 @@ function resetFilters() {
 }
 
 
-function openModal(videoSrc, videoTitle, videoAuthor) {
+function openModal(videoSrc, videoTitle, videoAuthor, videoStars) {
     const modalVideoSource = document.getElementById('modalVideoSource');
     modalVideoSource.src = videoSrc;
     const modalVideo = document.getElementById('modalVideo');
     modalVideo.load();
-    document.getElementById('videoModalLabel').innerText = videoTitle + " (" + videoAuthor + ")"
+    document.getElementById('videoModalLabel').innerText = videoTitle;
+    document.getElementById('star-number').innerText = videoStars;
     $('#videoModal').modal('show');
+    $('#star-logo').removeClass('fas fa-star');
+    $('#star-logo').addClass('far fa-star');
+    document.getElementById('profile-link').href="profile.html?"+videoAuthor;
+    document.getElementById('profile-link').innerText = "More About " + videoAuthor;
 };
+
+function updateStar() {
+    const stars = parseInt(document.getElementById('star-number').innerText);
+    document.getElementById('star-number').innerText = stars + 1;
+    $('#star-logo').removeClass('far fa-star');
+    $('#star-logo').addClass('fas fa-star');
+}
